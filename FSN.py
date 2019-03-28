f63f1524601b199fb075bb1b17efa1e53a3130db
yumehatoi <38152431+yumehatoi@users.noreply.github.com>
2019-03-28 19:32:18 +0800

    Add files via upload

--- FSN.py
+++ FSN.py
@@ -8,32 +8,60 @@ from datetime import datetime, time, date, timedelta
 from lxml import etree
 import requests
 from bs4 import BeautifulSoup
+from random import choice
 
 
-def getHtmlText():
+def getHtmlText(proxy, filmname):
     header = {'User-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv,2.0.1) Gecko/20100101 Firefox/4.0.1'}
-    r = requests.get('https://www.smcinema.com/Schedule/PreferenceBranch?movieName=Fate%2FStay%20Night%3A%20Heaven%E2%80%99s%20Feel%20II%20-%20Lost%20Butterfly', headers = header)
+    r = requests.get('https://www.smcinema.com/Schedule/PreferenceBranch?movieName=' + filmname, headers = header, proxies = proxy)
     #r = requests.get('https://www.smcinema.com/Schedule/PreferenceBranch?movieName=Backtrace', headers = header)
     html = r.text
     soup = BeautifulSoup(html, 'lxml')
     branch = soup.find_all('option')
     return branch
 
+def getIppool (daili_url, daili_page):
+    IPpool = []
+    header = {'User-agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv,2.0.1) Gecko/20100101 Firefox/4.0.1'}
+    for page in range(1,daili_page):
+        html = requests.get(daili_url + str(page) + '/', headers = header).text
+        soup = BeautifulSoup(html, 'lxml')
+        table = soup.find_all('tr')
+        for tr in table:
+            try:
+                ip = tr.find_all('td')[0].text.strip()
+                port = tr.find_all('td')[1].text.strip()
+                IPpool.append('http://' + ip + ':' + port)
+            except:
+                pass
+    print(IPpool)
+    return IPpool
 
-def main ():
+def Toast():
     toaster = ToastNotifier()
-    branchs = getHtmlText()
+    return toaster.show_toast('Fate Stay Night')
+
+def main (daili_url, daili_page, filmname):
+    IPpool = getIppool(daili_url, daili_page)
+    proxy = {'http':choice(IPpool)}
+    branchs = getHtmlText(proxy, filmname)
     while branchs:
         if len(branchs) > 1:
             print('omedeto!')
-            driver = webdriver.Chrome()
-            driver.get('https://www.smcinema.com/Schedule?mn=Fate%2FStay%20Night%3A%20Heaven%E2%80%99s%20Feel%20II%20-%20Lost%20Butterfly')
-            toaster.show_toast('Fate Stay Night')
-            break
+            while True:
+                Toast()
+                sleep(5)
         else:
             print('Not this time', datetime.now())
-            branchs = getHtmlText()
+            proxy = {'http':choice(IPpool)}
+            branchs = getHtmlText(proxy, filmname)
+            print(proxy)
+            print(branchs)
             sleep(60)
 
+daili_url = 'https://www.kuaidaili.com/free/intr/'
+daili_page = 5
+filmname = 'Fate%2FStay%20Night%3A%20Heaven%E2%80%99s%20Feel%20II%20-%20Lost%20Butterfly'
+
 if __name__ == '__main__':
-    main()
+    main (daili_url, daili_page, filmname)
